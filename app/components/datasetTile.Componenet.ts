fb6e2c3f813f0e015b25d15866da1056ce6155e6
eapen john <eapeneapen@hotmail.com>
2016-06-08 07:20:00 +0100

    started accessing the input filelds

--- app/components/datasetTile.Componenet.ts
+++ app/components/datasetTile.Componenet.ts
@@ -2,7 +2,7 @@ import {Component} from 'angular2/core'
 
 @Component({
     selector:'dataset-tile',
-    template:'<div>Dataset1</div> <input (click)="addToBasket()" type="button"/>'
+    template:"<div>Dataset1</div> <input type='text' #comment>  <input (click)='addToBasket(comment)' value='add to basket' type='button'/>"
 })
 
 export class DatasetTile
@@ -12,9 +12,9 @@ export class DatasetTile
         console.log('hi i am from datasettile')
     }
     
-    addToBasket()
+    addToBasket(comment)
     {
-        console.log('add to basket clciked');
+        console.log('add to basket clciked' , comment.value);
     }
     
     
