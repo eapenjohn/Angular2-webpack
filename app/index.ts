cb613c6c59f83a94ee97b6b18321ae5d8a016fdd
eapen john <eapeneapen@hotmail.com>
2016-07-11 07:40:31 +0100

    removed unwanted references

--- app/index.ts
+++ app/index.ts
@@ -2,11 +2,6 @@
 import 'reflect-metadata';
 import {bootstrap} from '@angular/platform-browser-dynamic'
 import {Component} from '@angular/core'
-import {DatasetTile}  from './components/datasetTile.Componenet'
-import {DatasetListComponenet}  from './components/datasetList.component'
-import {CreateDataset}  from './components/createDataset.componenet'
-import {DatasetService} from './services/dataset.service'
-import {DatasetSelectorComponent} from './components/datasetSelector.component'
 
 import {APP_ROUTER_PROVIDERS} from './routes'
 import {RouterConfig,provideRouter,ROUTER_DIRECTIVES} from '@angular/router'
@@ -34,4 +29,4 @@ export class App {
      this.selection=$event;
      }
 }
-bootstrap(App, [APP_ROUTER_PROVIDERS,DatasetService]).catch(err => console.error(err));
+bootstrap(App, [APP_ROUTER_PROVIDERS]).catch(err => console.error(err));
