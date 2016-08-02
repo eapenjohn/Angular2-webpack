ee5efae9bdf0fca790dd9c404511db6e854900c6
eapen john <eapeneapen@hotmail.com>
2016-08-02 08:23:06 +0100

    different ways to use the route.navigate

--- app/components/dataset/list/list.component.ts
+++ app/components/dataset/list/list.component.ts
@@ -1,30 +1,48 @@
-import {Component,Input} from '@angular/core'
+import {
+  Component,
+  Input
+} from '@angular/core'
 
-import {DatasetService} from '../service/dataset.service'
-import {SearchPipe} from './pipe/search.pipe'
-import { ROUTER_DIRECTIVES } from '@angular/router';
+import {
+  DatasetService
+} from '../service/dataset.service'
+import {
+  SearchPipe
+} from './pipe/search.pipe'
+import {
+  ROUTER_DIRECTIVES,
+  Router,
+  ActivatedRoute
+} from '@angular/router';
 
 @Component({
-    selector:'dataset-lists',
-    templateUrl:'/app/components/dataset/list/list.html',
-    providers:[DatasetService],
-    pipes:[SearchPipe],
-    directives :[ROUTER_DIRECTIVES]
+  selector: 'dataset-lists',
+  templateUrl: '/app/components/dataset/list/list.html',
+  providers: [DatasetService],
+  pipes: [SearchPipe],
+  directives: [ROUTER_DIRECTIVES]
 })
 
-export class ListsComponent
-{
-     searchValue=''
-   // @Input() searchValue;
-    datasets;
- 
- constructor(datasetservice:DatasetService)
- {
-   this.datasets=datasetservice.get().then((response)=>
-   {
-     this.datasets=response;
-   });
- }
+export class ListsComponent {
+  searchValue = ''
+    // @Input() searchValue;
+  datasets;
+
+  constructor(datasetservice: DatasetService, private router: Router, private route: ActivatedRoute) {
+    this.datasets = datasetservice.get().then((response) => {
+      this.datasets = response;
+    });
+  }
+
+  navigateToView(dataset) {
+    // this.router.navigateByUrl('/dataset/' + dataset.id)
+
+    //  this.router.navigate(['dataset', dataset.id]);
+
+    this.router.navigate([dataset.id], {
+      relativeTo: this.route
+    });
+  }
 }
 
-ListsComponent['parameters']=[DatasetService]
+ListsComponent['parameters'] = [DatasetService, Router, ActivatedRoute]
