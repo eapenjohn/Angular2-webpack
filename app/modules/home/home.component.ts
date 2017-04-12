b4d434e38a64c09462c0fb1750d8c4f3e16aafa7
eapen john <eapen.thekkekandathil@adstream.com>
2017-04-12 07:45:41 +0100

    started using mdIconRegistery

--- app/modules/home/home.component.ts
+++ app/modules/home/home.component.ts
@@ -6,18 +6,22 @@ import {
     Router
 } from '@angular/router'
 
+import { MdIconRegistry } from '@angular/material'
+
+import { DomSanitizer } from "@angular/platform-browser"
 
 @Component({
     selector: 'home',
     template: require('./home.html'),
 })
 export class HomeComponent {
-    constructor(private router) {
-
+    constructor(private router, private mdIconRegistry: MdIconRegistry, private domSanitizer: DomSanitizer) {
+        mdIconRegistry.addSvgIcon("share", domSanitizer.bypassSecurityTrustResourceUrl("https://rawgit.com/angular/material-start/es5-tutorial/app/assets/svg/share.svg"));
+        mdIconRegistry.addSvgIconSet(domSanitizer.bypassSecurityTrustResourceUrl("https://rawgit.com/angular/material-start/es5-tutorial/app/assets/svg/avatars.svg"))
     }
     isplayDeatils() {
         this.router.navigateByUrl('(details:details-items;id:1)')
     }
 }
 
-HomeComponent['parameters'] = [Router]
+HomeComponent['parameters'] = [Router, MdIconRegistry, DomSanitizer]
